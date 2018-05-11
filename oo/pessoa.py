1d60e1e09bb4703830f41904cf3ccaae3450e3a9
Isaac Ferreira Filho <isaacmob@riseup.net>
2018-05-11 20:18:43 -0300

    criado atributo complexo filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,21 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=33):
+    def __init__(self, *filhos, nome=None, idade=33):
         self.idade = idade
-        self.nome = None
-
+        self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Isaac'
-    print(p.nome)
-    print(p.idade)
-    p.idade = 78
-    print(p.idade)
+    isaac = Pessoa(nome='Isaac')
+    tobias = Pessoa(nome='Tobias')
+    manolo = Pessoa(isaac, tobias, nome='Manolo')
+    print(Pessoa.cumprimentar(manolo))
+    print(id(manolo))
+    print(manolo.cumprimentar())
+    print(manolo.nome)
+    print(manolo.idade)
+    for filho in manolo.filhos:
+        print(filho.nome)
