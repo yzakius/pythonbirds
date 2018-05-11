aeef8a26453d9560c6101e40c21f05fff344943b
Isaac Ferreira Filho <isaacmob@riseup.net>
2018-05-11 19:55:59 -0300

    criados atributos de instância nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,21 @@
 class Pessoa:
+    def __init__(self, nome = None, idade=33):
+        self.idade = idade
+        self.nome = None
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Isaac'
+    print(p.nome)
+    print(p.idade)
+    p.idade = 78
+    print(p.idade)
